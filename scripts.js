e1bc22571629c0a7a9df39b886a63efd91947107
Nick <nsardonini@gmail.com>
2020-02-17 23:07:04 -0500

    added input bar, submit button, and reset button. added styles to all three. added container to hold them in HTML

--- scripts.js
+++ scripts.js
@@ -7,10 +7,31 @@ function makeRows(rows, cols) {
     container.style.setProperty('--gridCols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      
       cell.style.border = '1px solid gray';
       container.appendChild(cell).className = "gridItem";
     };
-  };
+};
   // Default setting upon page load
   makeRows(16, 16);
+
+const inputContainer = document.getElementById('userInputs');
+const button = document.createElement('button'); 
+const buttonHeight = button.style.height = '37px';
+const buttonWidth = button.style.width = '6em';
+const buttonText = button.innerText = 'Submit';
+const buttonPadding = button.style.marginLeft = '8px';
+
+const clearButton = document.createElement('button');
+const clearButtonHeight = clearButton.style.height = '37px';
+const clearButtonWidth = clearButton.style.width = '6em';
+const clearButtonText = clearButton.innerText = 'Start over';
+const clearButtonPadding = clearButton.style.marginLeft = '8px';
+
+let textBar = document.createElement('input');
+const textBarHeight = textBar.style.height = '37px';
+const textBarWidth = textBar.style.width = '160px';
+const textBarContent = textBar.placeholder = 'Select a grid size!'
+
+inputContainer.appendChild(textBar);
+inputContainer.appendChild(button);
+inputContainer.appendChild(clearButton);
